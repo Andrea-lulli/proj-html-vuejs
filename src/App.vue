fd565fe30d941263ec8bead5093a1d02ce9979ce
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-12-02 12:44:07 +0100

    creati data header e footer

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">   
+  <div id="app">
     <HeaderComp />
     <JumbotronComp />
     <MainComp />
@@ -8,27 +8,150 @@
 </template>
 
 <script>
-import HeaderComp from './components/HeaderComp.vue';
-import JumbotronComp from './components/JumbotronComp.vue';
-import MainComp from './components/MainComp.vue';
-import FooterComp from './components/FooterComp.vue';
+import HeaderComp from "./components/HeaderComp.vue";
+import JumbotronComp from "./components/JumbotronComp.vue";
+import MainComp from "./components/MainComp.vue";
+import FooterComp from "./components/FooterComp.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HeaderComp,
     JumbotronComp,
     MainComp,
     FooterComp,
   },
-    data() {
+  data() {
     return {
-     
+      nav: [
+        {
+          id: 1,
+          name: 'Home',
+        },
+        {
+          id: 2,
+          name: 'About us',
+        },
+        {
+          id: 3,
+          name: 'Feature',
+        },
+        {
+          id: 4,
+          name: 'Testimonials',
+        },
+        {
+          id: 5,
+          name: 'Contact US',
+        },
+      ],
 
+      Testimonials:[
+        {
+          id: 1,
+          photo: '',
+          name: 'Julia',
+          firstname: 'Aann',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
+        },
+         {
+          id: 2,
+          photo: '',
+          name: 'Mac',
+          firstname: 'Znder',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
+        }
+      ],
+      footer:[
+        {
+          id: 1,
+          name: 'Menu',
+          options_menu:[
+            {
+              id: 1,
+              name: 'About us',
+            },
+            {
+              id: 2,
+              name: 'Theme',
+            },
+            {
+              id: 3,
+              name: 'Features',
+            },
+            {
+              id: 4,
+              name: 'Pricing',
+            },
+            {
+              id: 5,
+              name: 'Blog',
+            },
+
+          ]
+
+        },
+        {
+          id: 2,
+          name: 'About Us',
+          options_menu:[
+            {
+              id: 1,
+              name: 'About Solfbox',
+            },
+            {
+              id: 2,
+              name: 'Roadmap',
+            },
+            {
+              id: 3,
+              name: 'How it Work',
+            },
+            {
+              id: 4,
+              name: 'Team',
+            },
+            {
+              id: 5,
+              name: 'News',
+            },
+
+          ]
+
+        },
+        {
+          id: 3,
+          name:'Quick Links',
+          options_menu:[
+            {
+              id: 1,
+              name: 'About us',
+            },
+            {
+              id: 2,
+              name: 'Theme',
+            },
+            {
+              id: 3,
+              name: 'Features',
+            },
+            {
+              id: 4,
+              name: 'Pricing',
+            },
+            {
+              id: 5,
+              name: 'Blog',
+            },
+
+          ]
+
+        }
+
+      ]
     };
-  }
-  
-}
+  },
+};
 </script>
 
 <style lang="scss">
@@ -36,12 +159,11 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  
 }
 
-*{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
