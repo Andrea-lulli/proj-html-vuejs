7c90b6685542ba449dc2ac5a5903d5b91a0e4d44
Andrea-Lulli <andrea.lulli89@gmail.com>
2022-12-02 12:52:38 +0100

    installato fontawesome

--- src/App.vue
+++ src/App.vue
@@ -52,6 +52,7 @@ export default {
           photo: '',
           name: 'Julia',
           firstname: 'Aann',
+          role: 'COO Softbox',
           description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
         },
          {
@@ -59,6 +60,7 @@ export default {
           photo: '',
           name: 'Mac',
           firstname: 'Znder',
+          role: 'Manager Sofbox',
           description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
         }
       ],
